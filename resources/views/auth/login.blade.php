fd1fb1453e85812464f4ed6b2533d4a36f5d83ed
Muhammad Ashraf <forazkar@gmail.com>
2023-08-25 23:13:49 +0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+  .background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1; /* Lower value to push it to the background */
+}
+
+</style>
+
+<svg class="background" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <defs>
+            <!-- Define diamond pattern -->
+            <pattern id="diamond" width="50" height="50" patternUnits="userSpaceOnUse">
+                <polygon points="25,0 50,25 25,50 0,25" fill="lightgray" />
+            </pattern>
+            <pattern id="diamond2" width="50" height="50" patternUnits="userSpaceOnUse">
+                <polygon points="25,0 50,25 25,50 0,25" fill="gold" />
+            </pattern>
+
+        </defs>
+        <rect x="1100" y="000" width="300" height="500" fill="url(#diamond2)" />
+        <path fill="lightblue" d="M0,160L48,144C96,128,192,96,288,90.7C384,85,480,107,576,144C672,181,768,235,864,234.7C960,235,1056,181,1152,176C1248,171,1344,213,1392,234.7L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+       
+
+        <!-- Apply patterns to shapes -->
+        <rect x="10" y="0" width="300" height="500" fill="url(#diamond)" />
+       
+
+</svg>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header" style="background-color: bisque">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -70,4 +103,5 @@
         </div>
     </div>
 </div>
+
 @endsection
